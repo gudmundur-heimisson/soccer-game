3925c43348fa8f3a6fb8450452082c0d9f7895f9
Gudmundur Heimisson <gudmundur.heimisson@gmail.com>
2017-01-12 00:29:42 -0800

    Add y inverse function

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from soccer_ball import x, y
+from soccer_ball import x, y, y_inv
 from random import random
 
 def data_gen(t=0):
     x1, x2 = 2, 0.5
     y1, y2, y3 = -1, 1.3, 0.3
+    t0 = y_inv(0.1, y1, y2, y3)
+    X0, Y0 = x(t0, x1, x2), y(t0, y1, y2, y3)
     while True:
-        t += 0.05
+        t += 0.01
         X, Y = x(t, x1, x2), y(t, y1, y2, y3)
         if Y < 0.1:
             y3 = Y
@@ -15,7 +17,9 @@ def data_gen(t=0):
             t = 0
             x2 = X
             x1 = -3 + 6*random()
-        yield X, Y
+            t0 = y_inv(0.1, y1, y2, y3)
+            X0, Y0 = x(t0, x1, x2), y(t, y1, y2, y3)
+        yield X, Y, X0, Y0
 
 def init():
     ax.set_ylim(0, 1)
@@ -26,13 +30,14 @@ def init():
     return line,
 
 fig, ax = plt.subplots()
-line, = ax.plot([], [], lw=2)
+line, = ax.plot([], [], marker='o')
+terminus, = ax.plot([0.5], [0], marker='o', ms=15)
 ax.grid()
 xdata, ydata = [], []
 
 def run(data):
     # update the data
-    X, Y = data
+    X, Y, X0, Y0 = data
     xdata.append(X)
     ydata.append(Y)
     if len(xdata) >= 10:
@@ -40,6 +45,7 @@ def run(data):
         del ydata[:1]
     xmin, xmax = ax.get_xlim()
     line.set_data(xdata, ydata)
+    terminus.set_data([X0], [Y0])
     return line,
 
 ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10,
--- soccer_ball.py
+++ soccer_ball.py
@@ -1,7 +1,10 @@
-from math import pow, floor
+from math import pow, floor, sqrt
 
 def y(t, c1, c2, c3):
     return c1 * pow(t,2) + c2 * t + c3 
+
+def y_inv(y, c1, c2, c3):
+    return (-c2 - sqrt(pow(c2, 2) - 4 * c1 * (c3-y))) / (2 * c1)
 
 def X(t, c1, c2):
     return c1 * t + c2 
